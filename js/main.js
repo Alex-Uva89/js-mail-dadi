e15eb4857d6c6bd951d1e2a4feb58194111998a8
Alex-Uva89 <a.uva1988@gmail.com>
2022-02-24 16:55:20 +0100

    Created structure and js indication

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+::::::::::::DATI::::::::::::::::
+
+// CHIEDIAMO I DATI ALL'UTENTE E CREIAMO NOSTRA COSTANTE
+
+Qual è la tua mail?
+
+// ::::::::::INPUT: GENERAZIONE NUMERI CASUALI::::::::::
+
+// creiamo ciclo
+for (let i = 0; i < array.length; i++) {
+    // const element = array[i];
+    
+}
+
+// SE VINCO IO "HAI VINTO"
+if
+
+// SE VINCE PC "HA VINTO IL PC"
+else
+
+// FINE
+
+
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
